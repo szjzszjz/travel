b07ade899b218a12cf18a2670174e51b1e06591f
史博 <312685889@qq.com>
2019-08-13 23:32:40 +0800

    解除事件的绑定

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -12,7 +12,7 @@
         </div>
       </div>
     </div>
-    <common-gallery :imgs = imgs v-show="showGallery" @hiddenGallery="hiddenGallery"></common-gallery>
+    <common-gallery :imgs = imgs v-if="showGallery" @hiddenGallery="hiddenGallery"></common-gallery>
   </div>
 </template>
 
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,16 +1,15 @@
 <template>
-    <div id="header">
-      <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
-        <span class="iconfont">&#xeb99;</span>
-      </router-link>
+  <div id="header">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <span class="iconfont">&#xeb99;</span>
+    </router-link>
 
-      <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
-        <router-link to="/">
-          <div class="iconfont header-fixed-back">&#xeb99;</div>
-        </router-link>
-        景点详情
-      </div>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xeb99;</div>
+      </router-link>景点详情
     </div>
+  </div>
 </template>
 
 <script>
@@ -26,6 +25,7 @@ export default {
   },
   methods: {
     handelScroll () {
+      console.log('croll')
       const offset = document.documentElement.scrollTop
       if (offset > 64) {
         console.log('>>>64')
@@ -43,49 +43,58 @@ export default {
       console.log(document.documentElement.scrollTop)
     }
   },
+  // 渲染界面的时候执行该周期函数
   activated () {
+    // 对所有的界面绑定监听事件
     window.addEventListener('scroll', this.handelScroll)
+  },
+  // 离开界面的时候执行
+  deactivated () {
+    // 当离开界面的时候解除事件的绑定
+    window.removeEventListener('scroll', this.handelScroll)
   }
 }
 </script>
 <style lang="stylus" scoped>
-@import "~styles/varibles.styl"
-  /*#header{
-    position: relative
-  }*/
-  .header-abs{
-    position absolute
-    top:.5rem
-    left:.5rem
-    height:2rem
-    width 2rem
-    border-radius: 50%
-    background-color: rgba(0,0,0,0.3)
-  }
-  .header-abs span{
-    padding: .5rem
-    line-height 2rem
-    text-align center
-    color: #ffffff
-  }
+@import '~styles/varibles.styl';
 
-  .header-fixed{
-    position fixed
-    top:0
-    left:0
-    right:0
-    line-height $headerHeight
-    text-align center
-    height:$headerHeight
-    background-color: $bgColor
-    color: $darkTextColor
-    font-size 1.2rem
-  }
-  .header-fixed-back{
-    position absolute
-    top:0
-    left:0
-    width $headerHeight
-  }
+// #header{
+// position: relative
+// }
+.header-abs {
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.3);
+}
 
+.header-abs span {
+  padding: 0.5rem;
+  line-height: 2rem;
+  text-align: center;
+  color: #ffffff;
+}
+
+.header-fixed {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  line-height: $headerHeight;
+  text-align: center;
+  height: $headerHeight;
+  background-color: $bgColor;
+  color: $darkTextColor;
+  font-size: 1.2rem;
+}
+
+.header-fixed-back {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: $headerHeight;
+}
 </style>
